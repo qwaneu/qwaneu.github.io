2bfd405c17e62ba63f9150de29631b7edaea9158
Willem van den Ende <willem@qwan.eu>
2016-05-06 14:05:24 +0100

    Set correct H(ost) for jekyll port forwarding

--- jekyll_server.sh
+++ jekyll_server.sh
@@ -2,4 +2,4 @@
 #Exports port 4000, but server reset
 #It will update the content in _site on a change, and you need to manually refresh the browser.
 
-docker run --name jekyll -p 4000:4000 --rm -v "$PWD:/src" grahamc/jekyll serve
+docker run --name jekyll -p 4000:4000 --rm -v "$PWD:/src" grahamc/jekyll serve -H 0.0.0.0
