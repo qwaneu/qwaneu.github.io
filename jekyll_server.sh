537dd3fec73f97dce2f3d4c36e661f193600f97e
Willem van den Ende <willem@livingsoftware.co.uk>
2018-01-18 13:35:08 +0000

    Containers no longer working or needed
    works with default ruby version in ubuntu 16.04, installs packages in
    project directory (bundle install), so containers obsolete.

--- jekyll_server.sh
+++ jekyll_server.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-#Exports port 4000, but server reset
-#It will update the content in _site on a change, and you need to manually refresh the browser.
+#!/bin/sh
 
-docker run --name jekyll -p 4000:4000 --rm -v "$PWD:/src" grahamc/jekyll serve -H 0.0.0.0
+bundle exec jekyll s $@
